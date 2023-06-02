c1b557186c53d1b874a21b258e9adbfa842bc5fb
Dietmar Eggemann <dietmar.eggemann@arm.com>
2023-06-02 22:02:10 +0000

    torture: Add lock_torture_writer_fifo module param
    Modifies locktorture writer to run as RT task.
    
    To use it:
    insmod /lib/modules/torture.ko random_shuffle=1
    insmod /lib/modules/locktorture.ko torture_type=mutex_lock rt_boost=1 rt_boost_factor=50 nested_locks=3 writer_fifo=1
    													^^^^^^^^^^^^^
    
    This patch has been helpful to uncover issues with the proxy-execution
    series.
    
    [ paulmck: Remove locktorture-specific code from kernel/torture.c. ]
    
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Josh Triplett <josh@joshtriplett.org>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: kernel-team@android.com
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    [jstultz: Include header change to build, reword commit message]
    Signed-off-by: John Stultz <jstultz@google.com>
    Acked-by: Davidlohr Bueso <dave@stgolabs.net>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/torture.c
+++ kernel/torture.c
@@ -37,6 +37,7 @@
 #include <linux/ktime.h>
 #include <asm/byteorder.h>
 #include <linux/torture.h>
+#include <linux/sched/rt.h>
 #include "rcu/rcu.h"
 
 MODULE_LICENSE("GPL");
@@ -56,6 +57,9 @@ module_param(verbose_sleep_duration, int, 0444);
 
 static int random_shuffle;
 module_param(random_shuffle, int, 0444);
+
+static int lock_torture_writer_fifo;
+module_param(lock_torture_writer_fifo, int, 0444);
 
 static char *torture_type;
 static int verbose;
@@ -734,7 +738,7 @@ bool stutter_wait(const char *title)
 	cond_resched_tasks_rcu_qs();
 	spt = READ_ONCE(stutter_pause_test);
 	for (; spt; spt = READ_ONCE(stutter_pause_test)) {
-		if (!ret) {
+		if (!ret && !rt_task(current)) {
 			sched_set_normal(current, MAX_NICE);
 			ret = true;
 		}
