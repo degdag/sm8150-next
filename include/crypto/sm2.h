0928b4a0bac259fdded3b20c49503cea3aeb33d3
Stephen Rothwell <sfr@canb.auug.org.au>
2023-06-27 16:16:05 +1000

    Revert "crypto: sm2 - Provide sm2_compute_z_digest when sm2 is disabled"
    This reverts commit 3867caee497edf6ce6b6117aac1c0b87c0a2cb5f.

--- include/crypto/sm2.h
+++ include/crypto/sm2.h
@@ -13,16 +13,7 @@
 
 struct shash_desc;
 
-#if IS_REACHABLE(CONFIG_CRYPTO_SM2)
 int sm2_compute_z_digest(struct shash_desc *desc,
 			 const void *key, unsigned int keylen, void *dgst);
-#else
-static inline int sm2_compute_z_digest(struct shash_desc *desc,
-				       const void *key, unsigned int keylen,
-				       void *dgst)
-{
-	return -ENOTSUPP;
-}
-#endif
 
 #endif /* _CRYPTO_SM2_H */
